8199d166d88b9c51e0f2099f9e26594e9b7b9d67
Clement <clementduchemin19@gmail.com>
2023-10-20 14:28:34 +0200

    cart delete total checkout

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -1,40 +1,5 @@
 
 
-/*document.addEventListener("DOMContentLoaded", function() {
-    let addToCartBtn = document.querySelector(".button-nfts");
-
-    if (addToCartBtn) {
-        let name = window.name;
-        let price = window.price;
-        let image_url = window.image_url;
-        console.log(name, price, image_url);
-
-        addToCartBtn.addEventListener("click", function() {
-            let cart = JSON.parse(localStorage.getItem("cart") || "[]");
-            let artworkItem = {
-                name,
-                image_url,
-                price,
-            };
-
-            cart.push(artworkItem);
-            localStorage.setItem("cart", JSON.stringify(cart));
-
-            console.log("Artwork item to add:", artworkItem);
-            console.log("Cart after addition:", cart);
-
-            alert("Artwork added to cart!");
-        });
-    }
-
-    let goToCartButton = document.getElementById('goToCartButton');
-    if (goToCartButton) {
-        goToCartButton.addEventListener('click', function() {
-            window.location.href = "/cart";
-        });
-    }
-});
-*/
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -44,7 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
         let name = window.name;
         let price = window.price;
         let image_url = window.image_url;
-        console.log(name, price, image_url);
+        let firstname = window.firstname;
+        let lastname = window.lastname
+        console.log(name, price, image_url, firstname, lastname);
 
         addToCartBtn.addEventListener("click", function() {
             let cart = JSON.parse(localStorage.getItem("cart") || "[]");
@@ -52,6 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 name,
                 image_url,
                 price,
+                firstname,
+                lastname
             };
 
             cart.push(artworkItem);
