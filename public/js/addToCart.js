0ba1985c79d1a7822453ee97898c2f4a1949d183
Clement <clementduchemin19@gmail.com>
2023-10-20 12:01:21 +0200

    cart 3

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -1,74 +1,83 @@
-/*document.addEventListener("DOMContentLoaded", function() {
 
-    // Get the add to cart button
+
+/*document.addEventListener("DOMContentLoaded", function() {
     let addToCartBtn = document.querySelector(".button-nfts");
 
-    // Get the image that you want to hide
-    let imageToHide = document.querySelector(".cd-single-img");
+    if (addToCartBtn) {
+        let name = window.name;
+        let price = window.price;
+        let image_url = window.image_url;
+        console.log(name, price, image_url);
 
-    addToCartBtn.addEventListener("click", function() {
-        // Hide the image
-        imageToHide.style.display = "none";
-    });
+        addToCartBtn.addEventListener("click", function() {
+            let cart = JSON.parse(localStorage.getItem("cart") || "[]");
+            let artworkItem = {
+                name,
+                image_url,
+                price,
+            };
 
+            cart.push(artworkItem);
+            localStorage.setItem("cart", JSON.stringify(cart));
+
+            console.log("Artwork item to add:", artworkItem);
+            console.log("Cart after addition:", cart);
+
+            alert("Artwork added to cart!");
+        });
+    }
+
+    let goToCartButton = document.getElementById('goToCartButton');
+    if (goToCartButton) {
+        goToCartButton.addEventListener('click', function() {
+            window.location.href = "/cart";
+        });
+    }
 });
 */
-/*
-document.addEventListener("DOMContentLoaded", function() {
-
-    let addToCartBtn = document.querySelector(".button-nfts");
-
-    addToCartBtn.addEventListener("click", function() {
 
 
-        let cart = JSON.parse(localStorage.getItem("cart") || "[]");
-
-        let artworkItem = {
-            id: artwork.id,
-            name: artwork.name,
-            imageUrl: artwork.image_url,
-            price: artwork.price
-        };
-
-        cart.push(artworkItem);
-
-        localStorage.setItem("cart", JSON.stringify(cart));
-
-        console.log("Artwork item to add:", artworkItem);
-        cart.push(artworkItem);
-        console.log("Cart after addition:", cart);
-
-        alert("Artwork added to cart!");
-    });
-
-});*/
 document.addEventListener("DOMContentLoaded", function() {
     let addToCartBtn = document.querySelector(".button-nfts");
-    let name = window.name; // Ensure artwork is defined globally on the window object.
-    let price = window.price
-    let image_url = window.image_url
-    console.log(name, price, image_url)
 
-    addToCartBtn.addEventListener("click", function() {
-        let cart = JSON.parse(localStorage.getItem("cart") || "[]");
-        let artworkItem = {
-            name,
-            image_url,
-            price,
-        };
+    if (addToCartBtn) {
+        let name = window.name;
+        let price = window.price;
+        let image_url = window.image_url;
+        console.log(name, price, image_url);
 
-        cart.push(artworkItem);
-        localStorage.setItem("cart", JSON.stringify(cart));
+        addToCartBtn.addEventListener("click", function() {
+            let cart = JSON.parse(localStorage.getItem("cart") || "[]");
+            let artworkItem = {
+                name,
+                image_url,
+                price,
+            };
 
-        console.log("Artwork item to add:", artworkItem);
-        console.log("Cart after addition:", cart);
+            cart.push(artworkItem);
+            localStorage.setItem("cart", JSON.stringify(cart));
 
-        alert("Artwork added to cart!");
-    });
+            console.log("Artwork item to add:", artworkItem);
+            console.log("Cart after addition:", cart);
+
+            alert("Artwork added to cart!");
+        });
+    }
+
+    let goToCartButton = document.getElementById('goToCartButton');
+    if (goToCartButton) {
+        goToCartButton.addEventListener('click', function() {
+            let cart = JSON.parse(localStorage.getItem("cart") || "[]");
+
+            if (cart.length === 0) {
+                alert("Your cart is empty.");
+            } else {
+                window.location.href = "/cart";
+            }
+        });
+    }
 });
 
-document.getElementById('goToCartButton').addEventListener('click', function() {
-    window.location.href = "/cart";
-});
+
 
 
--- public/js/displayCart.js
+++ public/js/displayCart.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     let cart = JSON.parse(localStorage.getItem("cart") || "[]");
     console.log("Retrieved cart:", cart);
     let cartContainer = document.querySelector('#cart-container');
@@ -13,7 +13,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
     cartContainer.innerHTML = cartHTML;
 
+});*/
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let cart = JSON.parse(localStorage.getItem("cart") || "[]");
+    console.log("Retrieved cart:", cart);
+    let cartContainer = document.querySelector('#cart-container');
+
+    let cartHTML = cart.map(artworkItem => `
+    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">  <!-- Bootstrap responsive columns -->
+        <div class="card mx-auto" style="width: 18rem;"> <!-- Bootstrap card -->
+            <img src="${artworkItem.image_url}" alt="${artworkItem.name}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${artworkItem.name}</h5>
+                <p class="card-text">${artworkItem.price} euros</p>
+            </div>
+        </div>
+    </div>
+`).join('');
+
+    cartContainer.innerHTML = `
+<div class="container cd-cart-container">
+    <div class="row justify-content-center">
+        ${cartHTML}
+    </div>
+</div>
+`;
 });
-
-
 
